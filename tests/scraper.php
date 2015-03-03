335ecb03eb22f0319670190a0f4f21e2debb9397
Simone Ruffino <simone.ruffino@telecomitalia.it>
2015-03-03 23:10:21 +0100

    ora printa i nomi dei giocatori con il voto

--- tests/scraper.php
+++ tests/scraper.php
@@ -1,68 +1,109 @@
 
 
-// <?php
-
-// # Use the Curl extension to query Google and get back a page of results
-// $url = "http://www.fantagazzetta.com";
-// $ch = curl_init();
-// $timeout = 5;
-// curl_setopt($ch, CURLOPT_URL, $url);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-// $html = curl_exec($ch);
-// curl_close($ch);
-// //echo $html;
-
-// # Create a DOM parser object
-// $dom = new DOMDocument();
-
-// # Parse the HTML.
-// # The @ before the method call suppresses any warnings that
-// # loadHTML might throw because of invalid HTML in the page.
-// @$dom->loadHTML($html);
-
-// # Iterate over all the <a> tags
-// foreach($dom->getElementsByTagName('a') as $link) {
-//         # Show the <a href>
-//         echo $link->getAttribute('href');
-//         echo "<br />";
-// }
-// ?>
-
-
 <?php 
 
-require_once 'simple_html_dom.php';
+// require_once 'simple_html_dom.php';
 require_once 'url_to_absolute.php';
 
-$url = "http://www.fantagazzetta.com";
-$html = file_get_html( $url );
 
-//$link = $html->find('a[title="Voti Serie A"]', 0);
-
-foreach ($html->find('a') as $element) {
-	echo url_to_absolute($url, $element->href), "<br>\n";
+function get_page($url)
+{
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+	$html = curl_exec($ch);
+	curl_close($ch);
+	return $html;
 }
 
+$url = "http://www.fantagazzetta.com";
+$html = get_page($url);
 
-echo "<br>SIMO<br>"
+# Create a DOM parser object
+$dom = new DOMDocument();
 
+# Parse the HTML.
+# The @ before the method call suppresses any warnings that
+# loadHTML might throw because of invalid HTML in the page.
+@$dom->loadHTML($html);
 
-// $url = $link->attr['href'];
-// $title = $link->innertext;
-// echo $title, "\n", $url, "\n\n<br>";
+// Empty array to hold all links to return
+$links = array();
 
+$pattern = '/voti-fanta/';
 
+# Iterate over all the <a> tags
+foreach($dom->getElementsByTagName('a') as $link) {
+    $links[] = array('url' => $link->getAttribute('href'), 'text' => $link->nodeValue);
+    # Show the <a href>
+    $l = $link->getAttribute('href');
+    if (preg_match($pattern, $l ,$matches)) {
+		// print_r($matches);
+		$link_voti = url_to_absolute($url, $l);
+		// echo $link_voti;
+    	// echo "< THIS IS A MATCH !!br />";
+    }
+}
 
+// echo $links[26]['url'];
 
-// $items = $html->find('li[class=first]');
+# retrieve voti page
+$url = $link_voti;
+$html = get_page($url);
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
 
-// foreach ($items as $item) {
-// 	$link = $item->find('a',0);
-// 	$url = $link->attr['href'];
-// 	$title = $link->innertext;
+#print all players names
 
-// 	echo $title, "\n", $url, "\n\n<br>";
+$xpath = new DOMXpath($dom);
+$player_names = $xpath->query('//td[@class="n"]');
+foreach ($player_names as $container) {
+	// print player name
+	$arr = $container->getElementsByTagName("a");
+	foreach ($arr as $link) {
+	    $l = $link->getAttribute('href');
+	    $t = $link->nodeValue;
+	    echo $t;
+	}
+	echo "&nbsp;&nbsp;";
+	
+	// next sibling: voto Milano
+	$voto_container = $container->nextSibling;
+	echo $voto_container->textContent, "<br />";
+	// check element type for class vamm, vesp etc. 
+	$p = $voto_container;
+	if ($p->hasAttributes()) {
+	  foreach ($p->attributes as $attr) {
+	    $name = $attr->nodeName;
+	    $value = $attr->nodeValue;
+	    echo "Attribute '$name' :: '$value'<br />";
+	  }
+	}
+
+	// Bonus calc.
+
+	// next sibling: Gf
+	// next sibling: Gr
+	// next sibling: Gs
+	// next sibling: Rp
+	// next sibling: Rs
+	// next sibling: Au, skip
+	// next sibling: As, skip
+	// next siblings (Roma): V, Gf, Gr, Gs, Rp, Rs, Au - all skipped
+	// next sibling: As
+}
+
+// foreach($dom->getElementsByTagName('a') as $link) {
+
+//     $l = $link->getAttribute('href');
+//     $t = $link->nodeValue;
+//     echo $l,"<br />";
 // }
 
+echo "<br>SIMO<br>";
+
 ?>
+
+
