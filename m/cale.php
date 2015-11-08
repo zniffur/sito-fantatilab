8815e5f46688c3c7fb2266e26e7606c8c46d5afa
Simone Ruffino <simone.ruffino@telecomitalia.it>
2015-11-08 17:59:45 +0100

    fix m calendario

--- m/cale.php
+++ m/cale.php
@@ -1,48 +1,38 @@
 ﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 
-<HTML xmlns="http://www.w3.org/1999/xhtml"><HEAD><TITLE>Fantacalcio - Calendario</TITLE>
-<META content="text/html; charset=utf-8" http-equiv=Content-Type>
-<META name=viewport 
-content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+<HTML xmlns="http://www.w3.org/1999/xhtml">
+  <HEAD>
+    <TITLE>Fantacalcio - Calendario</TITLE>
+    <META content="text/html; charset=utf-8" http-equiv=Content-Type/>
+    <META name=viewport
+    content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
+    <META name=GENERATOR content="MSHTML 8.00.7600.16466"/>
+    <link href="styles_mobile.css" rel="stylesheet" type="text/css"/>
+    <SCRIPT type=text/javascript src="../script.js"></SCRIPT>
 
-<SCRIPT type=text/javascript src="../js/script.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="../js/fcmVariabili.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="../js/fcmGenerale.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="../js/fcmUtils.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="../js/fcmLegaDati.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="js/fcmSerieAFunzioni.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="../js/fcmSerieADati.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="../js/fcmTabelle.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="js/fcmCompetizioniFunzioni.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="../js/fcmCompetizioniDati.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="js/fcmCalendarioFunzioni.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="../js/fcmCalendarioDati.js"></SCRIPT>
+    <SCRIPT type=text/javascript src="../menujs/dataA.js"></SCRIPT>
 
-<SCRIPT type=text/javascript src="../js/fcmVariabili.js"></SCRIPT>
 
-<SCRIPT type=text/javascript src="../js/fcmGenerale.js"></SCRIPT>
-
-<SCRIPT type=text/javascript src="../js/fcmUtils.js"></SCRIPT>
-
-<SCRIPT type=text/javascript src="../js/fcmLegaDati.js"></SCRIPT>
-
-<SCRIPT type=text/javascript src="js/fcmSerieAFunzioni.js"></SCRIPT>
-
-<SCRIPT type=text/javascript src="../js/fcmSerieADati.js"></SCRIPT>
-
-<SCRIPT type=text/javascript src="../js/fcmTabelle.js"></SCRIPT>
-
-<SCRIPT type=text/javascript 
-src="js/fcmCompetizioniFunzioni.js"></SCRIPT>
-
-<SCRIPT type=text/javascript src="../js/fcmCompetizioniDati.js"></SCRIPT>
-
-<SCRIPT type=text/javascript src="js/fcmCalendarioFunzioni.js"></SCRIPT>
-
-<SCRIPT type=text/javascript src="../js/fcmCalendarioDati.js"></SCRIPT>
-
-<SCRIPT type=text/javascript src="../js/DataA.js"></SCRIPT>
-
-<META name=GENERATOR content="MSHTML 8.00.7600.16466">
-<link href="styles_mobile.css" rel="stylesheet" type="text/css">
-</HEAD>
-<BODY>
+  </HEAD>
+  <BODY>
 <? include ('header.htm') ?>
 
 
 <TABLE width="100%" cellPadding=0 cellSpacing=0 class=f><TBODY>    <TR bgcolor="#FFFFFF">
                             <TD>
-                               <DIV class=nws_article><A class=nws_snipp_lst 
-         
+                               <DIV class=nws_article><A class=nws_snipp_lst
+
             href="cale.php">
             <H3>CALENDARIO</H3>
             <P>Calendario degli incontri</P>
