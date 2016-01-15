2186419eaefd73cd1b25426e9687124345708230
Simone Ruffino <simone.ruffino@telecomitalia.it>
2016-01-15 23:36:00 +0100

    mobile

--- m/index.php
+++ m/index.php
@@ -1,92 +1,107 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3c.org/TR/1999/REC-html401-19991224/loose.dtd">
 <!-- saved from url=(0032)http://mobile.fantagazzetta.com/ -->
 <HTML xmlns="http://www.w3.org/1999/xhtml">
+
 <HEAD>
-<TITLE>Fantacalcio - Sito Mobile</TITLE>
-<META content="text/html; charset=iso-8859-1" http-equiv=Content-Type>
-<META name=viewport 
-content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
-<LINK 
-rel=stylesheet type=text/css href="styles.css">
-<META name=GENERATOR content="MSHTML 8.00.7600.16625">
-<script src="../js/fcmLegaDati.js" type="text/javascript"></script>
-<link href="styles_mobile.css" rel="stylesheet" type="text/css">
+    <TITLE>Fantacalcio - Sito Mobile</TITLE>
+    <META content="text/html; charset=iso-8859-1" http-equiv=Content-Type>
+    <META name=viewport content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+    <LINK rel=stylesheet type=text/css href="styles.css">
+    <META name=GENERATOR content="MSHTML 8.00.7600.16625">
+    <script src="../js/fcmLegaDati.js" type="text/javascript"></script>
+    <link href="styles_mobile.css" rel="stylesheet" type="text/css">
 </HEAD>
+
 <BODY>
-<? include ('header.htm') ?>
-<div id="hdr-bnr" align=middle>
-  <H1><B><p align="left"><font color="#FFFFFF"><script language="JavaScript" type="text/javascript">
+    <? include ('header.htm') ?>
+        <div id="hdr-bnr" align=middle>
+            <H1><B><p align="left"><font color="#FFFFFF"><script language="JavaScript" type="text/javascript">
 document.write(nomelega)
 </script></font></p></B></H1>
-  <p align="left"><FONT class=bnr_nml_txt color="#FFFFFF"><B> <script language="JavaScript" type="text/javascript">
+            <p align="left">
+                <FONT class=bnr_nml_txt color="#FFFFFF"><B> <script language="JavaScript" type="text/javascript">
 document.write(stagione)
    </script>^ edizione | 
     <script language="JavaScript" type="text/javascript">
 document.write(anno)
    </script></B></FONT> </p>
-</div>
+        </div>
 
-<!-- Content Area Start -->
-<TABLE id=content cellSpacing=0 cellPadding=0 width="100%" align=center>
-  <TBODY>
-  
-    <TD vAlign=top align=left><DIV class=nws_article><A class=nws_snipp_lst 
-            href="rose.php">
-        <H3><img src="img/form.png" height="40" align="left">ROSE</H3>
-        <P>Giocatori a disposizione</P>
-        </A></DIV>
-      <DIV class=nws_article><A class=nws_snipp_lst href="class.php">
-        <H3><img src="img/pallone.png" alt="" height="40" align="left">CLASSIFICA</H3>
-        <P>Classifica aggiornata all'ultimo turno</P>
-        </A></DIV>
-      <DIV class=nws_article><A class=nws_snipp_lst 
-            title="Turni di campionato" 
-            href="cale.php">
-        <H3><img src="img/calendario.png" height="40" align="left">CALENDARIO</H3>
-        <P>Turni di campionato</P>
-        </A></DIV>
-      <DIV class=nws_article><A class=nws_snipp_lst href="invform.php">
-        <H3><img src="img/scarpa.png" height="40" align="left">INVIA FORMAZIONE</H3>
-        <P>Inserisci i giocatori per la prossima giornata</P>
-        </A></DIV>
-      <DIV class=nws_article><A class=nws_snipp_lst 
-     
-            href="form.php">
-        <H3><img src="img/campo.png" height="40" align="left">FORMAZIONI</H3>
-        <P>Elenco dei giocatori schierati per la prossima giornata</P>
-        </A></DIV>
-      <DIV class=nws_article><A class=nws_snipp_lst href="ris.php">
-        <H3><img src="img/fischietto.png" height="40" align="left">RISULTATI</H3>
-        <P>Punteggi ottenuti nell'ultima giornata</P>
-        </A></DIV>
-      <DIV class=nws_article><A class=nws_snipp_lst 
-            href="fantamister/fantamister.php">
-        <H3><img src="img/fantacalcio.png" height="40" align="left">FANTAMISTER</H3>
-        <P>Statistiche Fantamister</P>
-        </A></DIV>
-      <DIV class=nws_article><A class=nws_snipp_lst 
-            href="quote.php">
-        <H3><img src="img/scudetto.png" height="40" align="left">QUOTE</H3>
-        <P>Quote TotoSNAI</P>
-        </A></DIV>
-      <DIV class=nws_article><A class=nws_snipp_lst 
-            href="stats.php">
-        <H3><img src="img/grafico.png" height="40" align="left">STATISTICHE</H3>
-        <P>Statistiche</P>
-        </A></DIV>
-      <DIV class=nws_article><A class=nws_snipp_lst 
-            href="rv.php">
-        <H3><img src="img/matita.png" height="40" align="left">REGISTRO</H3>
-        <P>Registro voti</P>
-        </A></DIV></TD>
-  </TR>
-    </TBODY>
-</TABLE>
-<!-- Content Area End --> 
+        <!-- Content Area Start -->
+        <TABLE id=content cellSpacing=0 cellPadding=0 width="100%" align=center>
+            <TBODY>
 
-<br />
-<div align="center"> <a href="../">Versione Classica del Sito</a></div>
-<br />
-<div align="center" class="g">Sito Mobile basato su Bruskin by SuperKaze</div>
+                <TD vAlign=top align=left>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst href="rose.php">
+                            <H3><img src="img/form.png" height="40" align="left">ROSE</H3>
+                            <P>Giocatori a disposizione</P>
+                        </A>
+                    </DIV>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst href="class.php">
+                            <H3><img src="img/pallone.png" alt="" height="40" align="left">CLASSIFICA</H3>
+                            <P>Classifica aggiornata all'ultimo turno</P>
+                        </A>
+                    </DIV>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst title="Turni di campionato" href="cale.php">
+                            <H3><img src="img/calendario.png" height="40" align="left">CALENDARIO</H3>
+                            <P>Turni di campionato</P>
+                        </A>
+                    </DIV>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst href="invform.php">
+                            <H3><img src="img/scarpa.png" height="40" align="left">INVIA FORMAZIONE</H3>
+                            <P>Inserisci i giocatori per la prossima giornata</P>
+                        </A>
+                    </DIV>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst href="form.php">
+                            <H3><img src="img/campo.png" height="40" align="left">FORMAZIONI</H3>
+                            <P>Elenco dei giocatori schierati per la prossima giornata</P>
+                        </A>
+                    </DIV>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst href="ris.php">
+                            <H3><img src="img/fischietto.png" height="40" align="left">RISULTATI</H3>
+                            <P>Punteggi ottenuti nell'ultima giornata</P>
+                        </A>
+                    </DIV>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst href="fantamister/fantamister.php">
+                            <H3><img src="img/fantacalcio.png" height="40" align="left">FANTAMISTER</H3>
+                            <P>Statistiche Fantamister</P>
+                        </A>
+                    </DIV>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst href="stats.php">
+                            <H3><img src="img/grafico.png" height="40" align="left">STATISTICHE</H3>
+                            <P>Statistiche</P>
+                        </A>
+                    </DIV>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst href="rv.php">
+                            <H3><img src="img/matita.png" height="40" align="left">REGISTRO</H3>
+                            <P>Registro voti</P>
+                        </A>
+                    </DIV>
+                    <DIV class=nws_article>
+                        <A class=nws_snipp_lst href="../conti_cesso/conti_cesso.html">
+                            <H3><img src="img/scudetto.png" height="40" align="left">CONTI CESSO</H3>
+                            <P>Fai i conti comodamente seduto sulla tazza.</P>
+                        </A>
+                    </DIV>
+                </TD>
+                </TR>
+            </TBODY>
+        </TABLE>
+        <!-- Content Area End -->
+
+        <br />
+        <div align="center"> <a href="../">Versione Classica del Sito</a></div>
+        <br />
+        <div align="center" class="g">Sito Mobile basato su Bruskin by SuperKaze</div>
 </BODY>
+
 </HTML>
